7fa7b5ef6c1746282d5f5b13e417788fa826a58e
jacknel <mjacknelson19@gmail.com>
2023-02-02 19:55:21 -0600

    reworked database.js file

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,29 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+const dbPromise = openDB('jate', 1, {
+  upgrade(db) {
+    if (db.objectStoreNames.contains('jate')) {
+      console.log('jate database already exists');
+      return;
+    }
+    db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+    console.log('jate database created');
+  },
+});
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  store.put(content);
+  return tx.complete;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  return store.getAll();
+};
 
 initdb();
